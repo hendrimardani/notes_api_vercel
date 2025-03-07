f1deeda79e00bad5cf03f7b9e3333e8d911e756b
hendrimardani <hendrimardani290301@gmail.com>
2025-03-08 02:25:20 +0700

    test

--- src/server.js
+++ src/server.js
@@ -4,49 +4,58 @@ const NotesService = require('./services/inMemory/notesServices');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
 
+let server;
+
 const init = async () => {
-  const notesServices = new NotesService();
+  if (!server) {
+    const notesServices = new NotesService();
 
-  const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
-    routes: {
-      cors: {
-        origin: ['*'],
+    server = Hapi.server({
+      port: process.env.PORT || 3000,
+      host: '0.0.0.0',
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
       },
-    },
-  });
+    });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesServices,
-      validator: NotesValidator,
-    },
-  });
+    await server.register({
+      plugin: notes,
+      options: {
+        service: notesServices,
+        validator: NotesValidator,
+      },
+    });
 
-  server.ext('onPreResponse', (request, h) => {
-    // mendapatkan konteks response dari request
-    const { response } = request;
-  
-    // penanganan client error secara internal.
-    if (response instanceof ClientError) {
-      const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
-      });
-      newResponse.code(response.statusCode);
-      return newResponse;
-    }
-      
-    return h.continue;
-  });
+    server.ext('onPreResponse', (request, h) => {
+      const { response } = request;
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      return h.continue;
+    });
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    await server.initialize(); // Hanya inisialisasi tanpa start()
+  }
+  return server;
 };
 
-// init();
+// Handler untuk Vercel
+module.exports = async (req, res) => {
+  const server = await init();
+  
+  const response = await server.inject({
+    method: req.method,
+    url: req.url,
+    payload: req.body,
+    headers: req.headers
+  });
 
-// Ekspor fungsi init agar bisa dijalankan oleh Vercel
-module.exports = init;
+  res.status(response.statusCode).json(response.result);
+};
