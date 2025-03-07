ab7cfbaf2127bbae35c769f12b2db6457af03c8e
hendrimardani <hendrimardani290301@gmail.com>
2025-03-08 02:20:38 +0700

    host: localhost

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
