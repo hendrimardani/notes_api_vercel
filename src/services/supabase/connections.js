07de70461cd81ec149fb67d9617776f096b260d5
hendrimardani <hendrimardani290301@gmail.com>
2025-03-08 11:39:33 +0700

    test

--- src/services/supabase/connections.js
+++ src/services/supabase/connections.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://fahmwxazdiuwypqcsslz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhaG13eGF6ZGl1d3lwcWNzc2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzOTEzMTYsImV4cCI6MjA1Njk2NzMxNn0.ijpsL4dSFzn3tg9YyStYjXEsRuHlSgsJeWcstM7f-x0";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
